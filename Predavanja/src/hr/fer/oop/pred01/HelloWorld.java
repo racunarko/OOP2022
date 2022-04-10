9788d76552ee2301acb7147cf0b5101ba2a4887d
dominik <dominik.barukcic@fer.hr>
2022-04-10 16:09:09 +0200

    adding minimal changes

--- Predavanja/src/hr/fer/oop/pred01/HelloWorld.java
+++ Predavanja/src/hr/fer/oop/pred01/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println("adding changes to the program"); // pushing new changes to the rep
+        System.out.println("adding changes again"); // pushing again
     }
 }
