25520337852f766e9d3dfafb82b45300633cf1d1
dominik <dominik.barukcic@fer.hr>
2022-04-10 16:03:25 +0200

    adding minor changes

--- Predavanja/src/hr/fer/oop/pred01/HelloWorld.java
+++ Predavanja/src/hr/fer/oop/pred01/HelloWorld.java
@@ -3,5 +3,6 @@ package hr.fer.oop.pred01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("adding changes to the program"); // pushing new changes to the rep
     }
 }
