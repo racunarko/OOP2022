459b6943ba3b4ece0896d986052703518537dfd7
dominik <dominik.barukcic@fer.hr>
2022-04-14 11:11:13 +0200

    added MI task 1

--- Ispiti/Probni meduispit 2022/zadatak1/src/main/java/hr/fer/oop/zad1/Cherry.java
+++ Ispiti/Probni meduispit 2022/zadatak1/src/main/java/hr/fer/oop/zad1/Cherry.java
@@ -1,13 +1,24 @@
 package hr.fer.oop.zad1;
 
-class {
+class Cherry extends Fruit {
 	private boolean hasWorm;
-	
-	public Cherry(String species, int numberOfFruits, double productionPrice, boolean hasWorm) {
-		
-		
-	}
-	
-	
 
+	public Cherry(String species, int numberOfFruits, double productionPrice, boolean hasWorm) {
+		super(species, numberOfFruits, productionPrice);
+		this.hasWorm = hasWorm;
+	}
+
+	public boolean isHasWorm() {
+		return hasWorm;
+	}
+
+	public void setHasWorm(boolean hasWorm) {
+		this.hasWorm = hasWorm;
+	}
+
+	@Override
+	public double myMarketPrice() {
+		double pricePerFruit = getProductionPrice() / getNumberOfFruits();
+		return hasWorm ? 1.2 * pricePerFruit : 1.5 * pricePerFruit;
+	}
 }
--- Ispiti/Probni meduispit 2022/zadatak1/src/main/java/hr/fer/oop/zad1/Fruit.java
+++ Ispiti/Probni meduispit 2022/zadatak1/src/main/java/hr/fer/oop/zad1/Fruit.java
@@ -1,16 +1,44 @@
 package hr.fer.oop.zad1;
 
- {
-	private String species;
-	private int numberOfFruits;
-	private double productionPrice;
-	
-	public Fruit(String species , int numberOfFruits , double productionPrice ) {
-		this.species = species;
-		this.numberOfFruits = numberOfFruits;
-		this.productionPrice = productionPrice;
-	}
-	
-	
-	
+abstract class Fruit {
+    private String species;
+    private int numberOfFruits;
+    private double productionPrice;
+
+    public Fruit(String species, int numberOfFruits, double productionPrice) {
+        this.species = species;
+        this.numberOfFruits = numberOfFruits;
+        this.productionPrice = productionPrice;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public int getNumberOfFruits() {
+        return numberOfFruits;
+    }
+
+    public void setNumberOfFruits(int numberOfFruits) {
+        this.numberOfFruits = numberOfFruits;
+    }
+
+    public double getProductionPrice() {
+        return productionPrice;
+    }
+
+    public void setProductionPrice(double productionPrice) {
+        this.productionPrice = productionPrice;
+    }
+
+    public double productionPrice(int numberOfTrees) {
+        return numberOfTrees * productionPrice;
+    }
+
+    public abstract double myMarketPrice();
+
 }
